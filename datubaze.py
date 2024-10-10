5f09481a6b3640dbea0b7f23102bf6921b84c830
temperelais <149486977+temperelais@users.noreply.github.com>
2024-10-10 16:52:18 +0300

    made database file and python functions do edit db

--- datubaze.py
+++ datubaze.py
@@ -1,3 +1,25 @@
 import sqlite3
-
 conn = sqlite3.connect("./faili/my.db")
+
+def kverijs(vaicajums):
+    cur = conn.cursor()
+    cur.execute(vaicajums)
+    conn.commit()
+
+tabulas_dzesana = "DROP TABLE skoleni"
+
+tabulas_izveide = """
+CREATE TABLE IF NOT EXISTS skoleni(
+    id_skolenam INTEGER PRIMARY KEY AUTOINCREMENT,
+    vards TEXT
+    uzvards TEXT
+    vecums INTEGER
+)
+
+"""
+datu_pievienosana = """
+INSERT INTO SKOLENI ()
+"""
+
+kverijs(tabulas_izveide)
+
