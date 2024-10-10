8ee4d9e810342e0f2e54aef46510ba96c08fb2ad
temperelais <149486977+temperelais@users.noreply.github.com>
2024-10-10 16:58:46 +0300

    KAUTKAS

--- datubaze.py
+++ datubaze.py
@@ -11,15 +11,23 @@ tabulas_dzesana = "DROP TABLE skoleni"
 tabulas_izveide = """
 CREATE TABLE IF NOT EXISTS skoleni(
     id_skolenam INTEGER PRIMARY KEY AUTOINCREMENT,
-    vards TEXT
-    uzvards TEXT
+    vards TEXT,
+    uzvards TEXT,
     vecums INTEGER
 )
 
 """
 datu_pievienosana = """
-INSERT INTO SKOLENI ()
+INSERT INTO SKOLENI (vards, uzvards, vecums)
+VALUES("Anna", "Bērziņa", 18)
 """
 
-kverijs(tabulas_izveide)
+kverijs(datu_pievienosana)
+
+def datu_pieliksana(tabula, kolonnas, dati):
+    vaicajums = f"""
+    INSERT INTO {tabula} ({kolonnas})
+    VALUES({dati})
+    """
+    kverijs(va)
 
